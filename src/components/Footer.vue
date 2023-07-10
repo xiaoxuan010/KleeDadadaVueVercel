62b72ad79a5fe6a08a5a1808e0a5c704be46c9c0
xiaoxuan010 <xiaoxuan010n@qq.com>
2023-07-11 02:40:33 +0800

    Repairing

--- src/components/Footer.vue
+++
@@ -1,6 +0,0 @@
-				本网站调用<a style="text-decoration: none" target="_blank" href="http:/yuanshenweb.top/yuanshen/webs/KleeWeb/klee.html">原站</a
-				本网站调用<a style="text-decoration: none" target="_blank" href="http:/yuanshenweb.top/yuanshen/webs/KleeWeb/klee.html">原站</a
-				本网站调用<a style="text-decoration: none" target="_blank" href="http:/yuanshenweb.top/yuanshen/webs/KleeWeb/klee.html">原站</a
-				本网站调用<a style="text-decoration: none" target="_blank" href="http:/yuanshenweb.top/yuanshen/webs/KleeWeb/klee.html">原站</a
-				本网站调用<a style="text-decoration: none" target="_blank" href="http:/yuanshenweb.top/yuanshen/webs/KleeWeb/klee.html">原站</a
-				本网站调用<a style="text-decoration: none" target="_blank" href="http:/yuanshenweb.top/yuanshen/webs/KleeWeb/klee.html">原站</a
